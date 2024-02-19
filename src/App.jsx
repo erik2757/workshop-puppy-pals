da43cbd27de954b49f76009e9ee4ef2a549e23f2
Erik Rodriguez <erikrodriguez@Eriks-Air.lan>
2024-02-19 13:59:50 -0600

    functionApp completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
+import {puppyList} from './data.js'
+import { useState } from 'react' 
+
+
+console.log(puppyList)
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log('puppyList: ', puppyList)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+      { 
+   puppies.map((puppy) => {
+     return <p key={puppy.id}>{puppy.name}</p>
+   })
+}
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+   
     </>
   )
 }
 
-export default App
+export default App 
